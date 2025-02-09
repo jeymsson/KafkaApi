6e4a41ac2c5b0c4f159c8e02ccf1e548f1244d4c
Jeymsson Nogueira Falcao <jnfalcao@solarbr.com.br>
2025-02-09 17:20:35 -0300

    feat: add swagger document

--- App/Program.cs
+++ App/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(option => {
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "KafkaAPI", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo API v1");
+        c.RoutePrefix = string.Empty; // Serve the Swagger UI at the app's root
+    });
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +33,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
---
+++ README.md
@@ -0,0 +1,7 @@
+# KafkaApi
+
+## Objective
+- The objective is to demonstrate how to use the Kafka on a API to send and receive messages.
+
+## Links
+- [-Development](http://localhost:5152/scalar)
